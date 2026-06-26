0f48867e589fff86975f1da3c2d3b986cbcae531
Galo223344 <galomercer@gmail.com>
2026-06-26 18:38:36 -0300

    Mas desvalidación de cache

--- app/api/mis-compras/route.ts
+++ app/api/mis-compras/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { NextResponse } from "next/server";
 import { obtenerOrdenesDelComprador } from "@/lib/services/orders";
 import { auth } from "@clerk/nextjs/server";
